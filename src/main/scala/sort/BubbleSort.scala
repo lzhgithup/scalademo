aecf50bb9e1d6684c12e3940edd76e21925eca5c
lzh <l2633002549@163.com>
2020-06-11 09:00:53 +0800

    Functions that return functions

---
+++ src/main/scala/sort/BubbleSort.scala
@@ -0,0 +1,29 @@
+package sort
+
+object BubbleSort {
+
+
+
+
+
+
+
+
+
+
+
+  def bubbleSort(array: Array[Int]): Unit = {
+    val swap = (a:Int, b:Int) => {
+      var temp = a
+//      a = b
+//      b = temp
+    }
+
+    for (i <- 0 to array.length - 1) {
+      if (array(i) > array(i + 1)) {
+
+      }
+    }
+  }
+
+}
